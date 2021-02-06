c53f30ab6aa61232ed814a2579684cd277fcc837
Bznkxs <gogodu@gmail.com>
2021-02-06 18:42:33 +0800

    change into oval

--- codes/fungisim.py
+++ codes/fungisim.py
@@ -15,24 +15,43 @@ if torch.has_cuda:
     device = 'cuda'
     dfloat = torch.float16 # torch.float16
     dint = torch.int8
-d_birth = 3  # must be odd
-d_death = 3
+d_birth = 9  # must be odd
+d_death = 9
 
 variance = 1
 
 
+def make_circular(mat):
+    h, d = mat.shape[-2], mat.shape[-1]
+    ox, oy = h // 2, d // 2
+    a, b = ox, oy
+    for i in range(h):
+        for j in range(d):
+            x = i - ox
+            y = j - oy
+            if x ** 2 / a ** 2 + y ** 2 / b ** 2 <= 1:
+                #print(i, j)
+                mat[i, j] = 1
+            else:
+                mat[i, j] = 0
+    #print("over",mat)
+
+
 
 w_b = torch.ones(d_birth, d_birth, dtype=dfloat, device=device)
-w_b[d_birth//2, d_birth//2] = 0
+make_circular(w_b)
+#print(w_b)
 w_b = w_b.reshape(1, 1, d_birth, d_birth)
 
 w_d = torch.ones(d_death, d_death, dtype=dfloat, device=device)
-w_d[d_death//2, d_death//2] = 0
+make_circular(w_d)
 w_d = w_d.reshape(1, 1, d_death, d_death)
 
+print(w_b)
+print(w_d)
 
-tr = 1  # timestep per sec
-expected_expectancy = 10 * tr  # in timesteps
+tr = 20  # timestep per sec
+expected_expectancy = 1000 * tr  # in timesteps
 
 # death rate
 b = 1. / expected_expectancy
@@ -41,12 +60,12 @@ death_rate = b
 birth_rate = 1.2 / tr  # (fungi per sec) / (timestep per sec) = (fungi per timestep)
 r = birth_rate - death_rate
 
-K = d_death * d_death * 0.741
+K = w_d[0, 0].sum() * 0.741
 
-k = 100
-t = 100 * tr  # in timesteps
+k = 200
+t = 80 * tr  # in timesteps
 
-init_p = 0.01
+init_p = 0.003
 
 er = False
 def debug(*args, **kwargs):
@@ -66,16 +85,14 @@ def fungisim(a, age, t):
     for i in tqdm(range(t)):
         debug(i,":")
         debug("a\n", a)
-        debug("ages\n", age)
-        # dying stage
-        age -= 1
-        a[age <= 0] = 0
 
-        sums = torch.conv2d(a, w_d, padding=d_death//2)
-        sums_ = torch.conv2d(a, w_b, padding=d_birth//2)
+
+
+        sums = torch.conv2d(a, w_d, stride=1, padding=d_death//2)
+
         debug("sums\n", sums)
-
-        death_rates = r * sums / K  # float
+        # sums[sums == 0] = 0.1
+        death_rates = (sums > K) * (1 - K / sums)  # float
         debug("death\n", death_rates)
         death_rates[death_rates > 1.] = 1.  # death rate exceed
         rr = torch.rand((1, 1, k, k), device=device)
@@ -85,9 +102,11 @@ def fungisim(a, age, t):
         debug("sample\n", sample)
         a = a - (a * sample)
         debug("after a\n", a)
-        age[a < 0.1] = 0
-        #  birth stage
+        outs.append(trans(a))
+        #age[a < 0.1] = 0
 
+                #  birth stage
+        sums_ = torch.conv2d(a, w_b, padding=d_birth//2)
         birth_rates = sums_ * birth_rate / d_birth / d_birth  # float
         debug("births\n", birth_rates)
         birth_rates[birth_rates > 1.] = 1.  # birth rate exceed
@@ -97,10 +116,15 @@ def fungisim(a, age, t):
         debug("sample1\n", birth_rates + rr)
         sample = (birth_rates + rr).to(dint)
         sample = ((1-a) * sample)
-        age[sample > 0] = (torch.randn(age[sample > 0].shape, device=device) * expected_expectancy)
+        #age[sample > 0] = (torch.randn(age[sample > 0].shape, device=device) * expected_expectancy)
         a += sample
+        # outs.append(trans(a))
+        #debug("ages\n", age)
+        # dying stage
+        #age -= 1
+        #a[age <= 0] = 0
+
         debug("at last\n", a)
-        outs.append(trans(a))
     return outs
 
 def draw():
@@ -122,7 +146,7 @@ def draw():
     #pyplot.ioff()
     #pyplot.show()
     # write_gif(outs, 'output.gif')
-
+    print(nums)
 
 
 # draw()
