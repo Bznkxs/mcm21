cddaae70dab1deb6e71f6294b465799751a28367
Mu437 <example@email.com>
2021-02-08 19:02:41 +0800

    changes

--- codes/fungisim.py
+++ codes/fungisim.py
@@ -8,7 +8,7 @@ from scipy.signal import convolve2d
 import scipy.sparse as sparse
 from mpl_toolkits.mplot3d import Axes3D
 # conv
-
+from preprocess import data_conversion, c_curve
 
 
 eps = 1e-5
@@ -58,25 +58,144 @@ def make_circular(mat, h=None, d=None):
 
 # env consts
 d_birth = 9  # must be odd
-d_death =7
+d_death = 7
+# exp. settings
+tr = 10  # timestep per sec
+k = 100  # width = height
+tot_time = 1  # total time span in sec
+init_p = 0.09  # initial density
+# calculated exp. settings
+t = tot_time * tr  # in timesteps
 
-variance = 1
 n_fungi = 3
 
-_birth_rates = torch.tensor([[
-    2.22, 2.25, 2.15
-][:n_fungi]], dtype=dfloat, device=device).reshape((1, n_fungi, 1, 1))  # 1xnx1x1
+#_birth_rates = torch.tensor([[
+#    2.22, 2.25, 2.15
+#][:n_fungi]], dtype=dfloat, device=device).reshape((1, n_fungi, 1, 1))  # 1xnx1x1
+
 capacities = torch.tensor([[
     0.741, 0.541, 0.6
 ][:n_fungi]], dtype=dfloat, device=device).reshape((1, n_fungi, 1, 1))  # 1xnx1x1
 
-# exp. settings
-tr = 10  # timestep per sec
-k = 2000  # width = height
-tot_time = 250  # total time span in sec
-init_p = 0.01  # initial density
+# micro
+Me = 3
+Ms = 3
+M_t = torch.tensor([
+    [1, 0, 0],
+    [0, 1, 0,],
+    [0, 0, 1.,],
+], device=device, dtype=dfloat)[:Me, :Ms]  # Me x Ms
+M_g = torch.tensor([
+    [2, 0, 0],
+    [0, 3, 0],
+    [0, 0, 3]
+], device=device, dtype=dfloat)[:n_fungi, :Me] / tr
+# M_need = torch.tensor([
+#     [1, 0, 0],
+#     [0, 1, 0],
+#     [0, 0, 1]
+# ], device=device, dtype=dfloat)[:n_fungi, :Ms] / tr
+# M_toxic = torch.tensor([
+#     [0, 0, 1.0],
+#     [1.0, 0, 0],
+#     [0, 1.0, 0]
+# ], device=device, dtype=dfloat)[:n_fungi, :Ms] / tr
+concentration_subs = torch.tensor([0.6, 1, 1], device=device, dtype=dfloat)[:Me] / tr # Me
 
-# calculated env. consts
+fungi_list, tpcurve, decurve, battles = data_conversion()
+n_fungi = len(fungi_list)
+# birth_rates: we assume they have the same birth rate.
+_birth_rates = torch.ones(n_fungi, dtype=dfloat, device=device).reshape(1, n_fungi, 1, 1)
+Me = n_fungi
+Ms = n_fungi + 1
+# generation matrix.
+M_g = torch.eye(Me, device=device, dtype=dfloat) / tr
+# colors
+
+colors = np.random.random((n_fungi, 1, 1, 3)) * 0.9 + 0.1
+# toxics
+
+
+M_toxic = torch.zeros(Me, Ms, device=device, dtype=dfloat)
+consider_interaction = True
+if consider_interaction:
+    for i, s1 in enumerate(fungi_list):
+        for j, s2 in enumerate(fungi_list):
+            M_toxic[i, j] = battles[s2][s1] / 10
+
+print("-------------")
+print(M_toxic)
+print("-------------")
+
+M_toxic /= tr
+# M_toxic[M_toxic > 0] = 1/M_toxic[M_toxic > 0]
+
+
+# requirement in std environment
+M_need = torch.zeros(Me, Ms, device=device, dtype=dfloat)
+for i, fungi in enumerate(fungi_list):
+    M_need[i, -1] = decurve(fungi, tpcurve(fungi, 22, -0.5)) / 1.5
+M_need /= tr
+
+def load_settings(temperature, moisture):
+    global n_fungi, _birth_rates, capacities, Me, Ms, M_t, M_g, M_need, M_toxic, concentration_subs, colors
+    # we get hyphal extension rate using temperature and moisture
+    hyphal_extension_rate = {}
+    for fungi in fungi_list:
+        hyphal_extension_rate[fungi] = tpcurve(fungi, temperature, moisture)
+    # we convert hyphal extension rate to decomposition rate
+    decomposition_rate = {}
+    for fungi in fungi_list:
+        decomposition_rate[fungi] = decurve(fungi, hyphal_extension_rate[fungi])
+    # we represent decomposition rate as enzymatic activity
+
+    # enzyme transition matrix.
+    M_t = torch.zeros(Me, Ms, device=device, dtype=dfloat)
+    for i, fungi in enumerate(fungi_list):
+
+        M_t[i, i] = decomposition_rate[fungi]
+        M_t[i, -1] = decomposition_rate[fungi]
+
+    print(M_t)
+    print(M_g)
+    print(M_need)
+    print(M_toxic)
+
+
+
+    #times.append(time())
+    # concentration of substrate.
+    concentration_subs = torch.ones(Me, device=device, dtype=dfloat) * c_curve(torch.tensor(temperature),
+                                                                               torch.tensor(moisture)) / tr
+    #times.append(time())
+
+    # times.append(time())
+    # spend = []
+    # for i in range(len(times) - 1):
+    #     spend.append( times[i+1]-times[i])
+    # print(spend)
+    # print(">",times[-1]-times[0])
+    #print(times)
+temp = [22, 28, 28]
+moist = [-0.5, -0.5, -1]
+changes = [0, 10, 20]
+
+for i in range(len(changes)):
+    changes[i] *= tr
+
+def load_environment():
+    pass
+
+
+
+
+
+K = 1.
+birth_rate = 1.
+r = birth_rate
+M_r = torch.Tensor()
+M_absorb = torch.Tensor()
+
 w_b = torch.zeros(n_fungi, n_fungi, d_birth, d_birth, dtype=dfloat, device=device)
 for i in range(n_fungi):
     make_circular(w_b[i, i])
@@ -85,23 +204,56 @@ w_d = torch.zeros(n_fungi, n_fungi, d_death, d_death, dtype=dfloat, device=devic
 for i in range(n_fungi):
     make_circular(w_d[i, i])
 
-
-
 death_range = w_d[0, 0].sum()
 birth_range = w_b[0, 0].sum()
-K = death_range * capacities
-birth_rate = _birth_rates / tr  # (fungi per sec) / (timestep per sec) = (fungi per timestep)
-r = birth_rate
 
-# calculated exp. settings
-t = tot_time * tr  # in timesteps
+w_absorb0 = torch.zeros(Ms, n_fungi, d_death, d_death, dtype=dfloat, device=device)
+for j in range(Ms):
+    for i in range(n_fungi):
+        make_circular(w_absorb0[j, i])
 
-# colors
-colors = np.array([
-    [1,0,0],
-    [0,1,0],
-    [0,0,1]
-][:n_fungi]).reshape((n_fungi, 1, 1, 3))
+w_absorb = torch.zeros(Ms, n_fungi, d_death, d_death, dtype=dfloat, device=device)
+
+w_enzgen = torch.zeros(n_fungi, n_fungi, d_death, d_death, dtype=dfloat, device=device)
+for i in range(n_fungi):
+    make_circular(w_enzgen[i, i])
+w_enzgen /= death_range
+
+def setting_calculation():
+    #import time
+    global w_b, w_d, death_range, birth_range, K, birth_rate, r, t, M_toxic, M_r, M_absorb, w_enzgen, w_absorb
+    # calculated env. consts
+    #tw = time.time()
+    K = death_range * capacities
+    birth_rate = _birth_rates / tr  # (fungi per sec) / (timestep per sec) = (fungi per timestep)
+    r = birth_rate
+
+    #t00 = time.time()
+
+
+
+    #t001 = time.time()
+    M_r = M_need - M_toxic
+    #t01 = time.time()
+    M_absorb = M_need + ((M_toxic > 0) & (M_need == 0)) / tr
+
+
+
+    #t0=time.time()
+    for j in range(Ms):
+        for i in range(n_fungi):
+            w_absorb[j, i] = w_absorb0[j, i] * M_absorb[i, j]
+    #t1=time.time()
+    #print(t00-tw, t001-t00, t01-t001, t0-t01, t1-t0, t1-tw)
+
+
+
+def s_curve(x):  # -1 <------> 1
+    return (torch.sigmoid(x * 2) * 2 - 1)
+def act(x):
+    # k x k x Me
+
+    return (s_curve(x / concentration_subs) * (concentration_subs))
 
 # simulation
 def fungisim_macro(a, age, t):
@@ -219,60 +371,7 @@ def fungisim_macro(a, age, t):
 # - if not enough, die at a calculated probability
 # - toxic: let it be a sigmoid function
 
-Me = 3
-Ms = 3
-M_t = torch.tensor([
-    [1, 0, 0],
-    [0, 1, 0,],
-    [0, 0, 1.,],
-], device=device, dtype=dfloat)[:Me, :Ms]  # Me x Ms
-M_g = torch.tensor([
-    [2, 0, 0],
-    [0, 3, 0],
-    [0, 0, 3]
-], device=device, dtype=dfloat)[:n_fungi, :Me] / tr
 
-
-
-M_need = torch.tensor([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
-], device=device, dtype=dfloat)[:n_fungi, :Ms] / tr
-
-M_toxic = torch.tensor([
-    [0, 0, 1.0],
-    [1.0, 0, 0],
-    [0, 1.0, 0]
-], device=device, dtype=dfloat)[:n_fungi, :Ms] / tr
-
-M_toxic[M_toxic > 0] = 1/M_toxic[M_toxic > 0]
-M_r = M_need - M_toxic
-
-M_absorb = M_need + ((M_toxic > 0) & (M_need == 0))
-
-
-w_enzgen = torch.zeros(n_fungi, n_fungi, d_death, d_death, dtype=dfloat, device=device)
-for i in range(n_fungi):
-    make_circular(w_enzgen[i, i])
-w_enzgen /= death_range
-
-w_absorb = torch.zeros(Ms, n_fungi, d_death, d_death, dtype=dfloat, device=device)
-for j in range(Ms):
-    for i in range(n_fungi):
-        make_circular(w_absorb[j, i])
-        w_absorb[j, i] *= M_absorb[i, j]
-
-act_weight = torch.tensor([0.6, 1, 1],
-    device=device, dtype=dfloat)[:Me] / tr # Me
-
-
-def s_curve(x):  # -1 <------> 1
-    return (torch.sigmoid(x * 2) * 2 - 1)
-def act(x):
-    # k x k x Me
-
-    return (s_curve(x / act_weight) * (act_weight))
     # exit(1)
 def fungisim_micro(a, age, t):
     def trans(a):
@@ -287,7 +386,16 @@ def fungisim_micro(a, age, t):
         return [np.sum(out[j]) for j in range(n_fungi)]  # (a[0] * 255).repeat(3, 1, 1).cpu().numpy()
     i = -1
     outs = [trans(a)]
+    p = 0
     for i in tqdm(range(t)):
+        #import time
+        #t0 = time.time()
+        if len(changes) > p and changes[p] == i:
+            load_settings(temperature=temp[p], moisture=moist[p])
+            setting_calculation()
+            p+=1
+        #t1 = time.time()
+        #print(t1-t0)
         debug(i,":")
         debug("a\n", a)
 
@@ -414,6 +522,7 @@ def fungisim_micro(a, age, t):
     return outs
 
 def draw():
+
     ski = torch.zeros(1, n_fungi, k, k, dtype=dfloat, device=device)
     rand_map = torch.rand((1, n_fungi, k, k), device=device)
     max_indices = rand_map.view(n_fungi, -1).argmax(0)
@@ -436,6 +545,7 @@ def draw():
     mask = mask.to_dense().to(device)
     ski[0, :, rand_map <= init_p] = 1
     ski = ski * mask
+
     print([ski[0, i].sum() for i in range(n_fungi)])
     nums = fungisim_micro(ski, None, t)  # num a 2-d list
     nums = np.array(nums).T  # a matrix
@@ -445,9 +555,18 @@ def draw():
     #pyplot.ion()
 
     print(nums)
-
+    def getcolor(color):
+        r, g, b = color[0], color[1], color[2]
+        def h(x):
+            x = int(x * 255)
+            hh = hex(x)
+            hh = hh[2:]
+            if len(hh) == 1:
+                hh = '0' + hh
+            return hh
+        return '#' + h(r) + h(g) + h(b)
     for _i, num in enumerate(nums):
-        pyplot.plot(np.arange(0, t + 1) / tr, num)
+        pyplot.plot(np.arange(0, t + 1) / tr, num, c=getcolor(colors[_i, 0, 0]))
     pyplot.show()
 
         #pyplot.pause(0.01)
@@ -456,13 +575,9 @@ def draw():
     # write_gif(outs, 'output.gif')
 
 
-import sys
 
-# feeee = open('C:\mcm21\codes\output.txt', 'w')
 
-#sys.stdout = feeee
-#sys.stderr = feeee
-# draw()
-draw()
+if __name__ == '__main__':
+    draw()
 
 #feeee.close()
